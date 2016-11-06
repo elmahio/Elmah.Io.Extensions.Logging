262a9569d021caafd25534a7e7324c055481de0b
Thomas Ardal <thomasardal@gmail.com>
2016-11-06 14:31:42 +0100

    Switched to using Create method to avoid problems when logging json

--- Elmah.Io.Extensions.Logging/ElmahIoLogger.cs
+++ Elmah.Io.Extensions.Logging/ElmahIoLogger.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Elmah.Io.Client;
+using Elmah.Io.Client.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Elmah.Io.Extensions.Logging
 {
     public class ElmahIoLogger : ILogger
-     {
+    {
         private readonly IElmahioAPI _elmahioApi;
         private readonly Guid _logId;
         private readonly LogLevel _level;
@@ -16,38 +17,52 @@ namespace Elmah.Io.Extensions.Logging
             _level = level;
             _elmahioApi = ElmahioAPI.Create(apiKey);
         }
- 
-         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-         {
-             if (formatter == null)
-             {
-                 throw new ArgumentNullException(nameof(formatter));
-             }
- 
-             if (!IsEnabled(logLevel)) return;
- 
-             _elmahioApi.Messages.Log(_logId, exception, LogLevelToSeverity(logLevel), formatter(state, exception));
-         }
- 
-         public bool IsEnabled(LogLevel logLevel)
-         {
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (formatter == null)
+            {
+                throw new ArgumentNullException(nameof(formatter));
+            }
+
+            if (!IsEnabled(logLevel)) return;
+
+            var message = formatter(state, exception);
+            var createMessage = new CreateMessage
+            {
+                DateTime = DateTime.UtcNow,
+                Title = message,
+                Severity = LogLevelToSeverity(logLevel).ToString()
+            };
+            if (exception != null)
+            {
+                createMessage.Detail = exception.ToString();
+                createMessage.Data = exception.ToDataList();
+                createMessage.Type = exception.GetType().Name;
+            }
+
+            _elmahioApi.Messages.Create(_logId.ToString(), createMessage);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
             return logLevel >= _level;
-         }
- 
-         public IDisposable BeginScope<TState>(TState state)
-         {
-             if (state == null)
-             {
-                 throw new ArgumentNullException(nameof(state));
-             }
-             //TODO not working with async
-             return null;
-         }
- 
-         private Severity LogLevelToSeverity(LogLevel logLevel)
-         {
-             switch (logLevel)
-             {
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            if (state == null)
+            {
+                throw new ArgumentNullException(nameof(state));
+            }
+            //TODO not working with async
+            return null;
+        }
+
+        private Severity LogLevelToSeverity(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
                 case LogLevel.Critical:
                     return Severity.Fatal;
                 case LogLevel.Debug:
@@ -59,10 +74,10 @@ namespace Elmah.Io.Extensions.Logging
                 case LogLevel.Trace:
                     return Severity.Verbose;
                 case LogLevel.Warning:
-                     return Severity.Warning;
-                 default:
-                     return Severity.Information;
-             }
-         }
-     }
+                    return Severity.Warning;
+                default:
+                    return Severity.Information;
+            }
+        }
+    }
 }
