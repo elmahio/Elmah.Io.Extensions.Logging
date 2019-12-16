1fb78f1d7bfc1d557719fe51d1697c6616ad68e8
Thomas Ardal <thomasardal@gmail.com>
2019-12-16 09:19:40 +0100

    Call dispose in console sample instead of letting user wait. Dispose now stores any remaining messages to elmah.io

--- samples/Elmah.Io.Extensions.Logging.ConsoleCore2/Program.cs
+++ samples/Elmah.Io.Extensions.Logging.ConsoleCore2/Program.cs
@@ -22,15 +22,15 @@ namespace Elmah.Io.Extensions.Logging.ConsoleCore2
                 //options.Period = TimeSpan.FromSeconds(1);
             }));
 
-            var loggerFactory = services.BuildServiceProvider().GetService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger("elmah.io");
-            logger.LogInformation("Hello World");
-            logger.LogInformation("A message with {type} {hostname} {application} {user} {source} {method} {version} {url} {statusCode} and {serverVariables}",
-                "custom type", "custom hostname", "custom application", "custom user", "custom source", "custom method",
-                "custom version", "custom url", 500, new Dictionary<string, object> { { "UserAgent", "Me" } });
-
-            Console.WriteLine("Microsoft.Extensions.Logging doesn't have the concept of flushing know from other logging frameworks. Wait a few seconds for log messages to be written to elmah.io. Then press any key to exit.");
-            Console.ReadKey();
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger("elmah.io");
+                logger.LogInformation("Hello World");
+                logger.LogInformation("A message with {type} {hostname} {application} {user} {source} {method} {version} {url} {statusCode} and {serverVariables}",
+                    "custom type", "custom hostname", "custom application", "custom user", "custom source", "custom method",
+                    "custom version", "custom url", 500, new Dictionary<string, object> { { "UserAgent", "Me" } });
+            }
         }
     }
 }
