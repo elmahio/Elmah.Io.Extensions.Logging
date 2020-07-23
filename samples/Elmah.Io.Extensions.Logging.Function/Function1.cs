cbbe4a6799a7a2d17c011a05dcb3bb39e76e2626
Thomas Ardal <thomasardal@gmail.com>
2020-07-23 10:10:03 +0200

    Upgraded functions sample to v3

--- samples/Elmah.Io.Extensions.Logging.Function/Function1.cs
+++ samples/Elmah.Io.Extensions.Logging.Function/Function1.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Elmah.Io.Extensions.Logging.Function
