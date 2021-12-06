7b984402b2e322a8f4c06fab9cb65eb94322ecf2
Thomas Ardal <thomasardal@gmail.com>
2021-12-06 14:32:49 +0100

    New samples for .NET 6

---
+++ samples/Elmah.Io.Extensions.Logging.EntityFrameworkCore60/Program.cs
@@ -0,0 +1,23 @@
+﻿using Elmah.Io.Extensions.Logging.EntityFrameworkCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var db = new MyDatabase())
+{
+    db.Users.Add(new User { Name = "Mike Wheeler" });
+    db.Users.Add(new User { Name = "Dustin Henderson" });
+    db.Users.Add(new User { Name = "Lucas Sinclair" });
+    db.Users.Add(new User { Name = "Will Byers" });
+    db.Users.Add(new User { Name = "Eleven" });
+    db.SaveChanges();
+
+    try
+    {
+        db.Users.FromSqlRaw("SELECT * FROM USERS2").ToList(); // Causes an error since there is no table named 'USERS2'
+    }
+    catch
+    {
+
+    }
+}
+
+Console.ReadLine();
