7120f42f4f1b543397fc084099750d9c47f9fa8d
Thomas Ardal <thomasardal@gmail.com>
2023-08-21 12:58:20 +0200

    Added unit test of constructors in provider to avoid errors like the previously fixed one with options

---
+++ test/Elmah.Io.Extensions.Logging.Test/ElmahIoLoggerProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using System;
+
+namespace Elmah.Io.Extensions.Logging.Test
+{
+    public class ElmahIoLoggerProviderTest
+    {
+        [Test]
+        public void CanCreateInstanceWithOptions()
+        {
+            using var provider = new ElmahIoLoggerProvider(Options.Create(new ElmahIoProviderOptions()));
+            var logger = provider.CreateLogger("test");
+            Assert.NotNull(provider);
+            Assert.NotNull(logger);
+        }
+
+        [Test]
+        public void CanCreateInstanceWithOnlyRequiredParameters()
+        {
+            using var provider = new ElmahIoLoggerProvider("apiKey", Guid.NewGuid());
+            var logger = provider.CreateLogger("test");
+            Assert.NotNull(provider);
+            Assert.NotNull(logger);
+        }
+    }
+}
