1d2c860d79276aeae9f98d1d821a6c831e0cab9a
Mario <pagefault@users.noreply.github.com>
2026-03-12 14:21:03 +0100

    AspNetCore.ExtensionsLogging: add extension methods to control form data logging per endpoint

---
+++ src/Elmah.Io.AspNetCore.ExtensionsLogging/OptIntoElmahIoMultipartBodyLoggingMetadata.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Microsoft.AspNetCore.Builder
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+{
+    public class OptIntoElmahIoMultipartBodyLoggingMetadata : IOptIntoElmahIoMultipartBodyLoggingMetadata { }
+}
